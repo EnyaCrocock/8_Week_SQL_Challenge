ec6c0735f227c8280e7c5340f39e582915089a8b
Enya Crocock <94410139+EnyaCrocock@users.noreply.github.com>
2022-03-19 18:46:12 +0000

    Update and rename D_Pricing_and_Ratings_V1_Final.sql to D_Pricing_and_Ratings.sql

--- Case Study #2 - Pizza Runner/SQL/D_Pricing_and_Ratings.sql
+++ Case Study #2 - Pizza Runner/SQL/D_Pricing_and_Ratings.sql
@@ -4,11 +4,11 @@
 
 SELECT SUM( 
            CASE
-		       WHEN n.pizza_name = 'Meatlovers' 
-			   THEN 12
-			   ELSE 10
-		   END 
-		   ) AS total_earned
+	       WHEN n.pizza_name = 'Meatlovers' 
+	       THEN 12
+	       ELSE 10
+	   END 
+	   ) AS total_earned
 FROM  pizza_names AS n
 JOIN  ##customer_orders AS c
 ON    c.pizza_id = n.pizza_id
@@ -21,28 +21,28 @@ WHERE r.pickup_time IS NOT NULL
 
 WITH price AS (
                SELECT c.record_id,
-					  CASE 
-					      WHEN n.pizza_name = 'Meatlovers' 
-				          THEN 12
-				          ELSE 10
-			          END AS initial_price,
-					  SUM(
-					      CASE 
-					          WHEN x.extras_id <> '' 
-						      THEN 1
-						      ELSE 0
-					      END
-						  ) AS extra_cost
-			    FROM  ##customer_orders AS c
-				JOIN  pizza_names AS n
-				ON    c.pizza_id = n.pizza_id
-				JOIN  ##extras AS x
-				ON    x.record_id = c.record_id
-				JOIN  ##runner_orders AS r
+	              CASE 
+		          WHEN n.pizza_name = 'Meatlovers' 
+			  THEN 12
+			  ELSE 10
+		      END AS initial_price,
+		      SUM(
+		          CASE 
+			  WHEN x.extras_id <> '' 
+			  THEN 1
+			  ELSE 0
+		      END
+		      ) AS extra_cost
+		FROM  ##customer_orders AS c
+		JOIN  pizza_names AS n
+		ON    c.pizza_id = n.pizza_id
+		JOIN  ##extras AS x
+		ON    x.record_id = c.record_id
+		JOIN  ##runner_orders AS r
                 ON    c.order_id = r.order_id
                 WHERE r.pickup_time IS NOT NULL
-				GROUP BY c.record_id, n.pizza_name
-				)
+		GROUP BY c.record_id, n.pizza_name
+		)
 SELECT SUM(initial_price) + SUM(extra_cost) AS total_earned
 FROM   price
 
@@ -59,12 +59,12 @@ CREATE TABLE runner_ratings (
 INSERT INTO runner_ratings 
 VALUES (1, 5, 'perfect', '2020-01-01 20:00:00.000'),
        (2, 5, '', '2020-01-01 20:00:00.000'),
-	   (3, 3, 'runner got lost', '2020-01-03 02:02:00.000'),
-	   (4, 4, '', '2020-01-04 16:25:12.000'),
-	   (5, 2, 'came late and food was cold', '2020-01-08 23:03:00.000'),
-	   (7, 5, 'came sooner than expected', '2020-01-08 22:55:00.000'),
-	   (8, 4, '', '2020-01-10 01:00:00.000'),
-	   (10, 4, '', '2020-01-11 20:00:00.000') 
+       (3, 3, 'runner got lost', '2020-01-03 02:02:00.000'),
+       (4, 4, '', '2020-01-04 16:25:12.000'),
+       (5, 2, 'came late and food was cold', '2020-01-08 23:03:00.000'),
+       (7, 5, 'came sooner than expected', '2020-01-08 22:55:00.000'),
+       (8, 4, '', '2020-01-10 01:00:00.000'),
+       (10, 4, '', '2020-01-11 20:00:00.000') 
 
 SELECT *
 FROM   runner_ratings
@@ -83,14 +83,14 @@ FROM   runner_ratings
 
 SELECT c.customer_id,
        c.order_id,
-	   r.runner_id,
-	   a.rating,
-	   c.order_time,
-	   r.pickup_time,
-	   DATEDIFF(MINUTE, c.order_time, r.pickup_time) AS time_between_order_and_pickup,
-	   r.duration_minutes AS delivery_duration,
-	   CAST(distance_km / duration_minutes * 60 AS DECIMAL(4,2)) AS speed_km_h ,
-	   COUNT(c.pizza_id) AS total_pizzas
+       r.runner_id,
+       a.rating,
+       c.order_time,
+       r.pickup_time,
+       DATEDIFF(MINUTE, c.order_time, r.pickup_time) AS time_between_order_and_pickup,
+       r.duration_minutes AS delivery_duration,
+       CAST(distance_km / duration_minutes * 60 AS DECIMAL(4,2)) AS speed_km_h ,
+       COUNT(c.pizza_id) AS total_pizzas
 FROM   ##customer_orders AS c
 JOIN   ##runner_orders AS r
 ON     c.order_id = r.order_id
@@ -102,25 +102,25 @@ GROUP  BY c.customer_id, c.order_id, r.runner_id, a.rating, c.order_time, r.pick
 
 
 WITH finance AS (
-               SELECT c.order_id,
-					  SUM(
-					      CASE 
-					          WHEN n.pizza_name = 'Meatlovers' 
-				              THEN 12
-				              ELSE 10
-			              END
-						  ) AS pizza_revenue,
-					  CAST(0.3 * r.distance_km AS DECIMAL(4,2)) AS runner_pay
-			    FROM  ##customer_orders AS c
-				JOIN  pizza_names AS n
-				ON    c.pizza_id = n.pizza_id
-				JOIN  ##runner_orders AS r
-                ON    c.order_id = r.order_id
-                WHERE r.pickup_time IS NOT NULL
-				GROUP BY c.order_id, r.distance_km
-				)
+                 SELECT c.order_id,
+		 SUM(
+		     CASE 
+		         WHEN n.pizza_name = 'Meatlovers' 
+			 THEN 12
+			 ELSE 10
+		     END
+		     ) AS pizza_revenue,
+		     CAST(0.3 * r.distance_km AS DECIMAL(4,2)) AS runner_pay
+	       FROM  ##customer_orders AS c
+	       JOIN  pizza_names AS n
+	       ON    c.pizza_id = n.pizza_id
+	       JOIN  ##runner_orders AS r
+	       ON    c.order_id = r.order_id
+	       WHERE r.pickup_time IS NOT NULL
+	       GROUP BY c.order_id, r.distance_km
+	       )
 SELECT SUM(pizza_revenue) AS pizza_revenue,
-	   SUM(runner_pay) AS runner_cost,
+       SUM(runner_pay) AS runner_cost,
        SUM(pizza_revenue) - SUM(runner_pay) AS profit
 FROM   finance
 
